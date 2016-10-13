2a0a2c09971429e760ac7cda92267f48be10d642
Malachi Gray <graymd.cfi@gmail.com>
2016-10-13 08:02:21 -0400

    ch18 persisting data with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes'
 import Fish from './Fish'
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -12,11 +13,23 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+
     // getinitialstate
     this.state = {
       fishes: {},
       order: {}
     };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
 
     addFish(fish){
