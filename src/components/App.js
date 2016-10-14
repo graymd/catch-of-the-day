ce2d5a5e8f6c92bb7995cbcfe25e30a7e36ca8cf
Malachi Gray <malachigray@C02RC3QSG8WM.local>
2016-10-14 17:02:35 -0400

    ch21 removing items from state

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,10 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
 
     // getinitialstate
     this.state = {
@@ -64,6 +66,12 @@ class App extends React.Component {
   updateFish(key, updatedFish) {
     const fishes = {...this.state.fishes};
     fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
     this.setState({ fishes })
   }
 
@@ -73,14 +81,20 @@ class App extends React.Component {
       })
     }
 
-    addToOrder(key) {
-      //take a copy of our state
-      const order = {...this.state.order};
-      // update or add the new number of fish ordered
-      order[key] = order[key] +1 || 1;
-      //update our state
-      this.setState({ order });
-    }
+  addToOrder(key) {
+    //take a copy of our state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] +1 || 1;
+    //update our state
+    this.setState({ order });
+  }
+
+  removeFromOrder(key) {
+    const order = {...this.state.order};
+    delete order[key];
+    this.setState({ order });
+  }
 
   render(){
     return (
@@ -99,9 +113,11 @@ class App extends React.Component {
           fishes={this.state.fishes}
           order={this.state.order}
           params={this.props.params}
+          removeFromOrder={this.removeFromOrder}
         />
         <Inventory
           addFish={this.addFish}
+          removeFish={this.removeFish}
           loadSamples={this.loadSamples}
           fishes={this.state.fishes}
           updateFish={this.updateFish}
